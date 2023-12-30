6ba188aaf74a7bd799f043ee15511a29e49fe4ad
Pablo <pablito.federico@gmail.com>
2023-12-30 16:55:11 -0300

    Se agregan traducciones al español

---
+++ lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'La contraseña se ha restablecido!',
+    'sent' => 'Te hemos enviado un enlace para restablecer tu contraseña!',
+    'throttled' => 'Por favor aguarda antes de continuar.',
+    'token' => 'El token de restablecimiento de contraseña es inválido.',
+    'user' => "No podemos enocntrar a un usuario con esa cuenta de correo.",
+
+];
