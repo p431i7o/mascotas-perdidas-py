ff7880043b9cd18544a46a91c1dccee988e329b6
Pablo <pablito.federico@gmail.com>
2023-12-31 23:25:05 -0300

    Mejoras en los mensajes de email de verificacion y navbar para usuarios registrados/logueados

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,40 +1,20 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Formulario Login, mascotas Perdidas">
-        <meta name="author" content="Login,Mascotas Perdidas">
-        <title>Signin Template · Bootstrap v5.3</title>
-
-        @vite('resources/js/app.js')
-
-        <!-- Favicons -->
-        {{-- <link rel="apple-touch-icon" href="/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-        <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-        <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-        <link rel="manifest" href="/docs/5.3/assets/img/favicons/manifest.json">
-        <link rel="mask-icon" href="/docs/5.3/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
-        <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon.ico"> --}}
-        <meta name="theme-color" content="#712cf9">
-        
-        <!-- Custom styles for this template -->
-        <link href="sign-in.css" rel="stylesheet">
-    </head>
-    <body class="text-center">
+@extends('layouts.default')
+@section('content')
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
+            <div class="row justify-content-center mt-5">
+                <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-        
+
                         <div class="card-body">
-                            @if (session('resent'))
+                            @if (session('status') == 'verification-link-sent')
                                 <div class="alert alert-success" role="alert">
-                                    {{ __('A fresh verification link has been sent to your email address.') }}
+
+                                    Un nuevo enlace de verificacion de email te ha sido enviado!
                                 </div>
                             @endif
-        
+
+
                             {{ __('Before proceeding, please check your email for a verification link.') }}
                             {{ __('If you did not receive the email') }},
                             <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
@@ -46,5 +26,4 @@
                 </div>
             </div>
         </div>
-    </body>
-</html>
+@endsection
--- resources/views/layouts/parts/navbar.blade.php
+++ resources/views/layouts/parts/navbar.blade.php
@@ -27,30 +27,40 @@
                     echo 'active';
                 } ?>" href="<?= route('legal') ?>">Legal</a>
             </li>
-            <?php if(!session('sesion_iniciada')){ ?>
-            <li class="nav-item">
-                <a class="nav-link <?php if (Route::current()->getName() == 'register') {
-                    echo 'active';
-                } ?>" href="<?= route('register') ?>">Registro</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link <?php if (Route::has('login')) {
-                    echo 'active';
-                } ?>" href="<?= route('login') ?>">Iniciar Sesi&oacute;n</a>
-                <!-- tabindex="-1"  disabled aria-disabled="true" -->
-            </li>
-            <?php }else{ ?>
+
+                @auth
+                    <li class="nav-item">
+                        <a class="nav-link <?php if (Route::current()->getName() == 'reports.create') {
+                            echo 'active';
+                    } ?>" href="<?= route('reports.create') ?>">Nuevo reporte</a>
+                    </li>
+                @else
+                <li class="nav-item">
+                    <a class="nav-link <?php if (Route::has('login')) {
+                        echo 'active';
+                    } ?>" href="<?= route('login') ?>">Iniciar Sesi&oacute;n</a>
+                    <!-- tabindex="-1"  disabled aria-disabled="true" -->
+                </li>
+                    @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link <?php if (Route::current()->getName() == 'register') {
+                            echo 'active';
+                        } ?>" href="<?= route('register') ?>">Registro</a>
+                    </li>
+                    @endif
+                @endauth
             {{-- <li class="nav-item">
                 <a class="nav-link <?php if (Route::has('profile') ) {
                     echo 'active';
                 } ?>" href="<?= route('home') ?>">Mi cuenta</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= route('logout') ?>">Cerrar Sesi&oacute;n</a>
-                <!-- tabindex="-1"  disabled aria-disabled="true" -->
             </li> --}}
+            @auth
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= route('logout') ?>">Cerrar Sesi&oacute;n</a>
+                    <!-- tabindex="-1"  disabled aria-disabled="true" -->
+                </li>
+            @endauth
 
-            <?php } ?>
         </ul>
         <form action="<?= route('search') ?>" class="form-inline my-2 my-lg-0">
             <input name="search" class="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="Buscar">
