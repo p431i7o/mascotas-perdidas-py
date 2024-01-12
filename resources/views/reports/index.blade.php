69d108629d561e285790118842d3f0123e8fb91b
Pablo <pablito.federico@gmail.com>
2024-01-11 23:11:57 -0300

    Se agrega la vista para manejar la lista de reportes del usuario

---
+++ resources/views/reports/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.default')
+@section('content')
+<div class="container">
+    <div class="row text-center">
+        {{-- <h4 class="display-3 text-center">Inicio de Sesi&oacute;n</h4> --}}
+        <h4 class="display-3 text-center">Mis reportes</h4>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+    </div>
+</div>
+@endsection
+
+@push('scripts')
+    <script type="text/javascript">
+    </script>
+
+
+@endpush
+
+@push('styles')
+    <style>
+
+    </style>
+@endpush
