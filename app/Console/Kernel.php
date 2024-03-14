775f158aff2b2ee17b54951aee27a134d99d75c8
Pablo <pablito.federico@gmail.com>
2024-03-13 23:10:27 -0300

    Se agrega al schedule que se borre los reportes expirados con mas de n dias

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('expired-reports:delete')->daily();
     }
 
     /**
