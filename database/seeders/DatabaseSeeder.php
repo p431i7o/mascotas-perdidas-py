572fc1b5d1d9809b6d925cd1880631a6614e6ec8
p431i7o <pablito.federico@gmail.com>
2024-12-20 09:14:19 -0300

    Fix de los seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\AnimalKind;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
